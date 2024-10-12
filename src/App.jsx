02c39d120f5d6010e3035b94651deda0bdbe49af
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-10-12 13:18:59 -0300

    Solo aparece el contador, luego de la explosión aparecen la foto, la explosión y los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,17 @@ import { Box } from "@chakra-ui/react";
 import { CountdownTimer } from "./components/CountdownTimer";
 import { useState } from "react";
 
-
 function App() {
   const [showComponents, setShowComponents] = useState(false);
   const [showExplosion, setShowExplosion] = useState(false);
+  const [showPhoto, setShowPhoto] = useState(false);
 
   const handleFinish = () => {
     setShowExplosion(true);
+    setTimeout(() => {
+      setShowPhoto(true);
+    }, 500);
+
     setTimeout(() => {
       setShowComponents(true);
     }, 1500);
@@ -29,35 +33,41 @@ function App() {
         height="100vh"
         position="relative"
       >
-        {/* Mi Foto en el centro */}
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <img
-            src="/yo.png"
-            alt="Mi foto"
-            style={{ borderRadius: "50%", width: "150px", height: "150px" }}
-          />
-          <h1>Hola, soy [Tu Nombre]</h1>
-          <CountdownTimer onFinish={handleFinish} />
-        </Box>
 
-        {/* Efecto de explosión */}
+        {!showPhoto && (
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <CountdownTimer onFinish={handleFinish} />
+          </Box>
+        )}
+
+
+        {showPhoto && (
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <img
+              src="/yo.png"
+              alt="Mi foto"
+              style={{ borderRadius: "50%", width: "150px", height: "150px" }}
+            />
+            <h1>Hola, soy [Tu Nombre]</h1>
+          </Box>
+        )}
+
         {showExplosion && (
           <div className="absolute animate-waveExplosion bg-red-500 opacity-50 w-[150px] h-[150px] rounded-full"></div>
         )}
 
-        {/* Componentes que aparecen después de la explosión */}
         {showComponents && (
           <>
-            <Box position="absolute" top="20%" left="10%">
+            <Box position="absolute" top="10%" left="10%">
               <AboutMe />
             </Box>
-            <Box position="absolute" top="20%" right="10%">
+            <Box position="absolute" top="10%" right="10%">
               <Technologies />
             </Box>
-            <Box position="absolute" bottom="20%" left="10%">
+            <Box position="absolute" bottom="10%" left="10%">
               <OtherSkills />
             </Box>
-            <Box position="absolute" bottom="20%" right="10%">
+            <Box position="absolute" bottom="10%" right="10%">
               <Projects />
             </Box>
           </>
