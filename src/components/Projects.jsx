ac9fee52e342d9aa3fe54411c643617aaf360fc9
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-10-13 16:45:15 -0300

    Se modificó hover de Projects en modo oscuro

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,16 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, useColorModeValue } from "@chakra-ui/react";
 
 export const Projects = () => {
+  // Define el color de fondo para el hover según el modo de color actual
+  const hoverColor = useColorModeValue("gray.200", "gray.600");
+
   return (
     <Box
       borderWidth="1px"
       borderRadius="md"
       padding={4}
       textAlign="center"
-      _hover={{ bg: "gray.200" }}
+      _hover={{ bg: hoverColor }} // Aplicar color de fondo en hover
       transition="background-color 0.2s"
       cursor="pointer"
     >
