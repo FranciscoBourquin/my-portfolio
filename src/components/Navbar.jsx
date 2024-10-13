1a0819ff560b076aacae5af495eb541a766b30d5
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-10-13 16:00:17 -0300

    Se resuelve problema de visibilidad del navbar en modo oscuro

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,30 +5,28 @@ export const Navbar = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Box as="nav" px={4} py={2} boxShadow="sm" bg="white">
+    <Box as="nav" px={4} py={2} boxShadow="sm" bg={colorMode === "light" ? "white" : "gray.800"}>
       <Flex alignItems="center">
-
-        <Box fontSize="xl" fontWeight="bold">
+        <Box fontSize="xl" fontWeight="bold" color={colorMode === "light" ? "black" : "white"}>
           Mi Portfolio
         </Box>
 
         <Spacer />
 
         <HStack spacing={4}>
-
-          <Link href="#about" fontWeight="medium">
+          <Link href="#about" fontWeight="medium" color={colorMode === "light" ? "black" : "white"}>
             Sobre Mí
           </Link>
-          <Link href="#technologies" fontWeight="medium">
+          <Link href="#technologies" fontWeight="medium" color={colorMode === "light" ? "black" : "white"}>
             Tecnologías
           </Link>
-          <Link href="#skills" fontWeight="medium">
+          <Link href="#skills" fontWeight="medium" color={colorMode === "light" ? "black" : "white"}>
             Otras Habilidades
           </Link>
-          <Link href="#projects" fontWeight="medium">
+          <Link href="#projects" fontWeight="medium" color={colorMode === "light" ? "black" : "white"}>
             Proyectos
           </Link>
-          <Link href="#contact" fontWeight="medium">
+          <Link href="#contact" fontWeight="medium" color={colorMode === "light" ? "black" : "white"}>
             Contacto
           </Link>
 
@@ -37,6 +35,7 @@ export const Navbar = () => {
             icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
             onClick={toggleColorMode}
             variant="ghost"
+            colorScheme={colorMode === "light" ? "black" : "white"}
           />
 
           <Image src="/argentina.png" alt="Español" boxSize="24px" />
