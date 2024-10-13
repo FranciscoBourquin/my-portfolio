7dd4e132b0c1b162b162b4762fa29bcef62096fa
Francisco Bourquin <fran.bourquin89@gmail.com>
2024-10-13 16:39:23 -0300

    Se modificó hover de AboutMe en modo oscuro

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,33 +1,43 @@
-import { Box, Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  useColorMode,
+} from "@chakra-ui/react";
 
-// Componente para el título con estilos de OtherSkills
 export const AboutMeTitle = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode } = useColorMode();
 
   return (
     <>
-      {/* Título con efecto hover */}
+
       <Box
         borderWidth="1px"
         borderRadius="md"
         padding={4}
         textAlign="center"
-        _hover={{ bg: "gray.200" }}
+        _hover={{ bg: colorMode === "light" ? "gray.200" : "gray.600" }}
         transition="background-color 0.2s"
         cursor="pointer"
-        onClick={onOpen} // Abrir modal al hacer clic
+        onClick={onOpen}
       >
         <Text fontSize="xl">Sobre Mí</Text>
       </Box>
 
-      {/* Modal con contenido */}
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Sobre Mí</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            {/* Aquí se muestra el contenido al abrir el modal */}
+
             <AboutMeContent />
           </ModalBody>
         </ModalContent>
@@ -36,7 +46,6 @@ export const AboutMeTitle = () => {
   );
 };
 
-// Componente para el contenido dentro del modal
 export const AboutMeContent = () => {
   return (
     <Box mt={4} position="relative">
@@ -49,11 +58,11 @@ export const AboutMeContent = () => {
         margin="0 auto"
         position="relative"
       >
-        {/* Comilla superior izquierda */}
+
         <Box
           position="absolute"
-          top="10px" /* Ajuste dentro del borde */
-          left="10px" /* Ajuste dentro del borde */
+          top="10px"
+          left="10px"
           fontSize="4xl"
           fontWeight="bold"
           color="gray.400"
@@ -61,16 +70,14 @@ export const AboutMeContent = () => {
           “
         </Box>
 
-        {/* Texto del contenido */}
         <Text fontSize="lg">
           Soy un desarrollador apasionado por la tecnología y el aprendizaje continuo. Me encanta resolver problemas y crear soluciones eficientes a través del código.
         </Text>
 
-        {/* Comilla inferior derecha, ajustada más abajo */}
         <Box
           position="absolute"
-          bottom="-20px" /* Ajuste más abajo */
-          right="10px" /* Ajuste dentro del borde */
+          bottom="-20px"
+          right="10px"
           fontSize="4xl"
           fontWeight="bold"
           color="gray.400"
